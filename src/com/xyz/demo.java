1ad6dc9309a2f49222131c962243f158575c7644
Xyz <858596716@qq.com>
2018-12-12 19:41:10 +0800

    Initial commit

---
+++ src/com/xyz/demo.java
@@ -0,0 +1,11 @@
+package com.xyz;
+
+/**
+ * @author Xyz
+ * @create 2018-12-11-22:06
+ */
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("Helloworld..");
+    }
+}
